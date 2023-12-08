c899b4578fb365cac0f484662a6c2f2f4d42a2a5
LuiArm <Luisarmendariz24@icloud.com>
2023-12-07 21:25:37 -0700

    Initial Commit

---
+++ Earthquakes/EarthquakesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarthquakesApp.swift
+//  Earthquakes
+//
+//  Created by luis armendariz on 12/7/23.
+//
+
+import SwiftUI
+
+@main
+struct EarthquakesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
