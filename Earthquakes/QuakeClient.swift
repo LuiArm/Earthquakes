5be62d74601c19d47fde2d4b1fc9eea49616d77d
LuiArm <Luisarmendariz24@icloud.com>
2023-12-21 23:13:11 -0700

    finish network client

--- Earthquakes/QuakeClient.swift
+++ Earthquakes/QuakeClient.swift
@@ -9,6 +9,17 @@ import Foundation
 
 class QuakeClient {
     
+    //computed, asynchronous, throwing property to fetch the earthquakes
+    var quakes: [Quake] {
+        //Making a property asynchronous or throwing requires the explicit get syntax for a computed property.
+        get async throws {
+            let data = try await downloader.httpData(from: feedURL)
+            let allQuakes = try decoder.decode(GeoJSON.self, from: data)
+            return allQuakes.quakes
+        }
+    }
+    
+    
     // property for JSON decoder, using anonymous closure to initialize the property lets you change the date decoding strategy. Lazy will create decoder until its needed
     private lazy var decoder: JSONDecoder = {
         let aDecoder = JSONDecoder()
@@ -16,8 +27,14 @@ class QuakeClient {
         return aDecoder
     }()
     
+    //add downloader property
+    let downloader: any HTTPDataDownloader
     
+    //initializer to set the property value
+    init(downloader: any HTTPDataDownloader = URLSession.shared) {
+        self.downloader = downloader
+    }
     
     // url for feed
-    private let feedURL = URL(string: "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson")
+    private let feedURL = URL(string: "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson")!
 }
