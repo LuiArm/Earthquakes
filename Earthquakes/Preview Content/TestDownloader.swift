36cc9facca7240fe1e91bd14ce03d1eb68062d86
LuiArm <Luisarmendariz24@icloud.com>
2023-12-25 17:45:50 -0700

    finish test for quake fetching

--- Earthquakes/Preview Content/TestDownloader.swift
+++ Earthquakes/Preview Content/TestDownloader.swift
@@ -10,6 +10,8 @@ import Foundation
 
 class TestDownloader: HTTPDataDownloader {
     func httpData(from: URL) async throws -> Data {
-        fatalError("unimplemented")
+        //The call to Task.sleep(nanoseconds:) simulates network delay by sleeping for some amount of time.
+        try await Task.sleep(nanoseconds: UInt64.random(in: 100_000_000...500_000_000))
+        return testQuakesData
     }
 }
