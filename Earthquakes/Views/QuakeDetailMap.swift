bdcabdaffe4d628be4797bbae638866abc16310b
LuiArm <Luisarmendariz24@icloud.com>
2024-01-14 13:19:50 -0700

    map view

---
+++ Earthquakes/Views/QuakeDetailMap.swift
@@ -0,0 +1,18 @@
+//
+//  QuakeDetailMap.swift
+//  Earthquakes
+//
+//  Created by luis armendariz on 1/14/24.
+//
+
+import SwiftUI
+
+struct QuakeDetailMap: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuakeDetailMap()
+}
